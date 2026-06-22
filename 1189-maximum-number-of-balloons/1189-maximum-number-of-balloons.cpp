a7ffbab99b0f29395da99e2eddcd677eabccbb7f
Ramesh Kumar <128730622+Rameshsain070@users.noreply.github.com>
2026-06-22 12:36:18 +0530

    Time: 0 ms (100%), Space: 8.7 MB (99.49%) - LeetHub

---
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+        int n = text.length();
+        int bb = 0;
+        for(int i = 0; i < n; i++){
+            if(text[i] == 'b'){
+                bb++;
+            }
+        }
+        int aa = 0;
+        for(int i = 0; i < n; i++){
+            if(text[i] == 'a'){
+                aa++;
+            }
+        }
+        int le = 0;
+        for(int i = 0; i < n; i++){
+            if(text[i] == 'l'){
+                le++;
+            }
+        }
+        int oe = 0;
+        for(int i = 0; i < n; i++){
+            if(text[i] == 'o'){
+                oe++;
+            }
+        }
+        int nn = 0;
+        for(int i = 0; i < n; i++){
+            if(text[i] == 'n'){
+                nn++;
+            }
+        }
+
+        int min_ban = min({aa, bb, nn});
+        int min_lo = min(le,oe);
+
+        return min({bb, aa, nn, le / 2, oe / 2});
+
+    }
+};
